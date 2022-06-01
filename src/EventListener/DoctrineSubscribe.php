4bf43fb48ed2c003bd383e84e9f0b6bbc782dec0
maxime <max45270@gmail.com>
2022-06-01 15:03:35 +0200

    repotest

--- src/EventListener/DoctrineSubscribe.php
+++ src/EventListener/DoctrineSubscribe.php
@@ -2,31 +2,67 @@
 namespace App\EventListener;
 
 use App\Entity\User;
-use App\Repository\UserRepository;
-use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
 use Doctrine\ORM\Events;
+use Doctrine\ORM\EntityManager;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class DoctrineSubscribe implements EventSubscriberInterface
 {
+    protected $manager;
+    protected $userRepository;
+    
+    public function __construct(EntityManagerInterface $manager, UserRepository $userRepository)
+    {
+        $this->manager = $manager;
+        $this->userRepository = $userRepository;
+    }
+    
+    
     // this method can only return the event names; you cannot define a
     // custom method name to execute when each event triggers
     public function getSubscribedEvents(): array
     {
         return [
-            Events::postPersist,
+            Events::prePersist,
         ];
     }
 
-    public function postPersist(LifecycleEventArgs $args, UserRepository $userRepository) :void
+    public function prePersist(LifecycleEventArgs $args) :void
+    {
+        $this->changeNewUser($args);
+    }
+    
+    private function changeNewUser(LifecycleEventArgs $args) :void
     {
         $entity = $args->getObject();
-
+    
         if ($entity instanceof User) {
-            // dd($entity->getRoles());
-            dd($userRepository->findAll());
+            $lenghtArray = count($this->userRepository->findAll());
+            dump($entity->getRoles());
+            dump($this->userRepository->findAll());
+            // die;
+            if ($lenghtArray > 3){
+                dd('> à 3');
+            }else{
+                dd('< à 3');
+            }
+            // $this->userRepository->;
         };
+
     }
 }
+
+// class DoctrineAbstract extends AbstractController
+// {
+//     public function findAllUserRepo(UserRepository $userRepo)
+//     {
+//         return $userRepo->findAll();
+//     }
+// }
 
 ?>
